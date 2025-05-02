b426e639d8f22271981fec7563b9cbe3d2317678
Laurent Michel <lmichel@Sebastiens-MacBook-Pro.local>
2025-05-02 15:34:18 +0200

    install wezterm

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #! /bin/sh
 
+# homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /Users/lmichel/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/lmichel/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# wezterm
+brew install --cask wezterm
+
 
