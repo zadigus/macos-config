66994540a3c23823aea2300cde7ff08cd6715568
Laurent Michel <laurent.michel@cognex.com>
2025-05-05 09:58:56 +0200

    fix git config

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /Users/lmichel/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/lmichel/.zprofile
+echo >>/Users/lmichel/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/Users/lmichel/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update
 brew upgrade
@@ -26,12 +26,12 @@ brew install pass
 
 # zsh
 brew install powerlevel10k zsh-autosuggestions zsh-syntax-highlighting
-echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>~/.zshrc
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
+echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 source ~/.zshrc
 
-cat <<'EOF' >> ~/.zshrc
+cat <<'EOF' >>~/.zshrc
 HISTFILE=$HOME/.zhistory
 SAVEHIST=1000
 HISTSIZE=999
@@ -50,7 +50,7 @@ cp .tmux.conf ~/
 
 # eza
 brew install eza
-echo "alias ls=\"eza --icons=always\"" >> ~/.zshrc
+echo "alias ls=\"eza --icons=always\"" >>~/.zshrc
 
 # zoxide
 brew install zoxide
@@ -60,20 +60,25 @@ alias cd="z"
 # diverse utilities
 brew install wget yazi fzf rg fd
 
-# lazygit
+# git
 brew install lazygit
 cp lazygit-config.yml ~/Library/Application\ Support/lazygit/config.yml
+ssh-keygen -t ed25519 -b 4096 -C "laurent.michel@cognex.com" -f ~/.ssh/git_rsa
+cat <<EOF >>~/.zshrc
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/git_rsa
+EOF
 
 # python
 brew install xz pyenv pyenv-virtualenv
-cat <<'EOF' >> ~/.zshrc
+cat <<'EOF' >>~/.zshrc
 eval "$(pyenv init -)"
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 EOF
 source ~/.zshrc
 pyenv install 3.10.17 3.11.12 3.12.10 3.13.3
 for v in $(pyenv versions --bare); do
-  PYENV_VERSION=$v pip install ruff black flake8
+  PYENV_VERSION=$v pip install ruff black flake8
 done
 
 # terraform
